97cc6b0d4455441e109fc4d4c6d0ed2fa847b542
Harshad Dhonde <163443556+harshaddhonde4@users.noreply.github.com>
2024-10-31 20:20:56 +0530

    Add files via upload

---
+++ Assignment22.c
@@ -0,0 +1,34 @@
+/*
+Write A Program which Accepts Number from the User and Prints to that Number
+
+Example :
+I/P : 4
+O/P :
+1
+2
+3
+4
+*/
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= iNo ; iCnt++)
+    {
+        printf("%d\n", iCnt);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    printf("Enter the Number :\n");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
